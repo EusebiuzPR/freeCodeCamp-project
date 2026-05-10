6baa643bbf60ba7e69a1374aeaf561b5a74571ae
EusebiuzPR <273243314+EusebiuzPR@users.noreply.github.com>
2026-05-09 23:19:38 -0500

    Create a function to buil a factorial calculator on fCC

---
+++ jsProjects/factorialCalculator.js
@@ -0,0 +1,22 @@
+/*
+On this tiny project I had to create a function 
+to create a Factorial calculator... It was made on fCC "freeCodeCamp". 
+*/
+
+//// My Code
+
+let num = 5;
+
+function factorialCalculator(num) {
+  let result = 1;
+    for (let i = 1;i <= num;i++) {
+        result = result * i;
+    }
+        return result;
+}
+
+let factorial = factorialCalculator(num);
+
+let resultMsg = `Factorial of ${num} is ${factorial}`;
+
+console.log(resultMsg);
